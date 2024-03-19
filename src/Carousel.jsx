3226896a665aff981d4779e305745f0b6ddd3dc2
zen <zenon.deviagge@gmail.com>
2024-03-19 00:55:30 -0300

    Add logic to slide next and previous buttons. Test prevSlide and nextSlide functions to use as onClick attribute on button component.

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -4,18 +4,34 @@ import { FaQuoteRight } from 'react-icons/fa'
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
 
 const Carousel = () => {
-  const [people, setPeople] = useState(list);
+  const [people, setPeople] = useState(list)
+  const [currentPerson, setCurrentPerson] = useState(0)
 
-  const prevSlide = () => {}
-  const nextSlide = () => {}
+  const prevSlide = () => {
+        setCurrentPerson((oldPerson) => {
+          const result = (oldPerson - 1 + people.length) % people.length
+          return result
+        })
+  }
+  const nextSlide = () => {
+    setCurrentPerson((oldPerson)=>{
+      const result = (oldPerson + 1) % people.length
+      return result
+    })
+  }
 
   return (
     <section className="slider-container">
       {people.map((person, personIndex) => {
         const { id, image, name, title, quote } = person
         return (
-          <article className="slide" style=
-          {{transform:`translateX(${100 * personIndex}%)`}} key={id}>
+          <article
+            className="slide"
+            style={{
+              transform: `translateX(${100 * (personIndex - currentPerson)}%)`,
+            }}
+            key={id}
+          >
             <img src={image} alt={name} className="person-img" />
             <h5 className="name">{name}</h5>
             <p className="title">{title}</p>
